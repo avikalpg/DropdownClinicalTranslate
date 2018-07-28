2b7aff79a11c23db6ecd73a19adbfdd3fd06d55a
Avikalp Kumar Gupta <avikalp.gupta@freshgravity.com>
2018-07-28 09:21:12 +0530

    Installed ionic cordova and added builds for android and ios - they don't completely work yet

--- clinicAssist/src/pages/home/home.ts
+++ clinicAssist/src/pages/home/home.ts
@@ -7,6 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+	public textField:String = 'demo';
+	public x = 8;
+	public y = 3;
+	z = 5;
+
+
   constructor(public navCtrl: NavController) {
 
   }
