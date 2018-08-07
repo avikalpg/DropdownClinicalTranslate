2dc3f586c2d4422cb86bcc012f8568e4a0855875
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2018-08-07 08:34:37 +0530

    Added one auto-complete field on the home-page, which will be used as a template for the translator page auto-complete fields

--- clinicAssist/src/pages/home/home.ts
+++ clinicAssist/src/pages/home/home.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
@@ -12,9 +12,37 @@ export class HomePage {
 	public y = 3;
 	z = 5;
 
+	static responseOptions:string[] = [
+		"How are you?", 
+		"You are great!", 
+		"Option3", 
+		"Headache", 
+		"What is your age?"
+	];
 
-  constructor(public navCtrl: NavController) {
 
-  }
+	stringChange() {
+		console.log("---------------stringChange entered------------------------");
+	}
+
+	getResults(keyword:string) {
+		this.x = 1212;
+		console.log(HomePage.responseOptions);
+		console.log(this.x);
+		console.log(this.z);
+		// let responseOptions:string[] = ["How are you?", "You are great!", "Option3", "Headache", "What is your age?"];
+		return HomePage.responseOptions.filter(item => {
+			return item.toLowerCase().startsWith(keyword.toLowerCase())
+		});
+	}
+
+	constructor(public navCtrl: NavController) {
+
+
+		for (this.x = 0; this.x < HomePage.responseOptions.length; this.x++) {
+			console.log(HomePage.responseOptions[this.x]);
+		}
+
+	}
 
 }
