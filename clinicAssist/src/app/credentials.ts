76984cd8accb79fefa659e7e031f92f31713dc47
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2019-04-18 15:47:59 +0530

    Removed Firebase credentials.ts from source control and added it to .gitignore file

--- clinicAssist/src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCVmAaOVguDZHJVWcHt2xkBUIfN72P75MM',
-  authDomain: 'clinic-translator.firebaseapp.com',
-  databaseURL: 'https://clinic-translator.firebaseio.com',
-  projectId: 'clinic-translator',
-  storageBucket: 'clinic-translator.appspot.com',
-  messagingSenderId: '252479336509'
-};
